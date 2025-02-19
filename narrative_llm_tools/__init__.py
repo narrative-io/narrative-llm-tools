ef37a0b603b497a6a4f48f5801c08b1194ff183e
Nick Jordan <njordan@gmail.com>
2025-02-19 02:51:48 -0500

    fix: export reward functions

--- narrative_llm_tools/__init__.py
+++ narrative_llm_tools/__init__.py
@@ -7,6 +7,14 @@ from narrative_llm_tools.rest_api_client.types import (
     RestApiConfig,
     RestApiResponse,
 )
+from narrative_llm_tools.reward_functions.cot_with_tool_call import (
+    combine_rewards,
+    format_reward,
+    get_default_reward_function,
+    get_repetition_penalty_reward,
+    thought_steps_reward,
+    tool_calls_validity_reward,
+)
 from narrative_llm_tools.state.conversation_state import ConversationState
 from narrative_llm_tools.tools.json_schema_tools import JsonSchemaTools
 from narrative_llm_tools.utils.format_enforcer import get_format_enforcer
@@ -22,4 +30,10 @@ __all__ = [
     "get_format_enforcer",
     "ConversationState",
     "EndpointHandler",
+    "get_default_reward_function",
+    "thought_steps_reward",
+    "tool_calls_validity_reward",
+    "format_reward",
+    "get_repetition_penalty_reward",
+    "combine_rewards",
 ]
